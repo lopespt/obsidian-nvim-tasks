0e0271c017d662505fcbfc240fcd8b35315307aa
Guilherme Wachs Lopes <gwachs@uber.com>
2024-11-10 19:22:14 -0300

    plugin

--- lua/obsidian-nvim-tasks/init.lua
+++ lua/obsidian-nvim-tasks/init.lua
@@ -1,1 +1,10 @@
-print("teste")
+-- register a neovim function to print hello and attach  to command named :Hello
+
+local api = vim.api
+
+local function hello()
+  api.nvim_out_write("Hello, Neovim!\n")
+end
+
+api.nvim_create_user_command("Hello", hello, {})
+
